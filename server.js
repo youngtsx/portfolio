1feb95bdcb997dc4868e1c41b67a7fa290eef65e
youngtsx <youngtsx@gmail.com>
2025-11-10 21:18:41 -1000

    info

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const argv = require('minimist')(process.argv.slice(2));
-const express = require('express');
-const app = express();
-app.all('*', function (request, response, next) {
+var argv = require('minimist')(process.argv.slice(2));
+var express = require('express');
+var app = express();
+app.all(/.*/, function (request, response, next) {
     console.log(request.method + ' to path ' + request.path);
     next();
 });
-let root = (typeof argv["rootdir"] != "undefined")?argv["rootdir"] : ".";
-app.use(express.static( root ));
-let server = app.listen(8080, () => console.log(`listening on ${server.address().address} port ${server.address().port} rootdir ${root}`));
+app.use(express.static( (typeof argv["rootdir"] != "undefined")?argv["rootdir"] : "." ) );
+app.listen(8080, () => console.log(`listening on port 8080`));
